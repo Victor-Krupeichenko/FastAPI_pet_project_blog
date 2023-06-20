3ee07dd7cd9000d955010f0478079445d9b3ae54
Victor-Krupeichenko <71899727+Victor-Krupeichenko@users.noreply.github.com>
2023-06-20 16:54:23 +0300

    Адрес базы данных

---
+++ api_databases/address_db.py
@@ -0,0 +1,9 @@
+from src.settings_env import (
+    POSTGRES_USER as postg_user,
+    POSTGRES_PASSWORD as postg_password,
+    POSTGRES_HOST as postg_host,
+    POSTGRES_PORT as postg_port,
+    POSTGRES_DB as postg_db
+)
+
+URL_DATABASE = f'postgresql+asyncpg://{postg_user}:{postg_password}@{postg_host}:{postg_port}/{postg_db}'
