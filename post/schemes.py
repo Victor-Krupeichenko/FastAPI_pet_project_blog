4b609eead9db2e7bb27c7a3e491f6a8189046188
Victor-Krupeichenko <71899727+Victor-Krupeichenko@users.noreply.github.com>
2023-06-22 20:46:48 +0300

    Схемы для записей

---
+++ post/schemes.py
@@ -0,0 +1,11 @@
+from pydantic import BaseModel
+from typing import Text
+
+
+class PostScheme(BaseModel):
+    title: str
+    content: Text
+    category_id: int
+
+    class Config:
+        orm_mode = True
