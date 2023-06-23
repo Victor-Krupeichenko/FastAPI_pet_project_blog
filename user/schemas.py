9364b6d2e31d69a6b7e74ac8b398bdebc05c0d5d
Victor-Krupeichenko <71899727+Victor-Krupeichenko@users.noreply.github.com>
2023-06-23 18:37:55 +0300

    Схема для обновления поля группы у пользователя

--- user/schemas.py
+++ user/schemas.py
@@ -1,8 +1,10 @@
+from typing import Optional
 from pydantic import BaseModel, EmailStr, validator
 from email_validator import validate_email
 
 
 class UserSchema(BaseModel):
+    """Схема для создания пользователя"""
     username: str
     password: str
     email: EmailStr
@@ -11,6 +13,14 @@ class UserSchema(BaseModel):
     def validate_email(cls, email):
         validate_email(email)
         return email.lower()
+
+    class Config:
+        orm_mode = True
+
+
+class AdminUserScheme(BaseModel):
+    """Схема для обновления поля группы у пользователя"""
+    group: Optional[str] = "CLIENT"
 
     class Config:
         orm_mode = True
