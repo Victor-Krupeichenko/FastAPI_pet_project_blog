a4dc8b819c0a38c6cdd9d032fe74aa27e23b0a2f
Victor-Krupeichenko <71899727+Victor-Krupeichenko@users.noreply.github.com>
2023-06-27 23:05:24 +0300

    Схема для проверки данных при регистрации пользователя

--- user/schemas.py
+++ user/schemas.py
@@ -1,18 +1,45 @@
 from typing import Optional
-from pydantic import BaseModel, EmailStr, validator
-from email_validator import validate_email
+from fastapi import Form
+from pydantic import BaseModel, validator, errors
+from pydantic.networks import validate_email
 
 
 class UserSchema(BaseModel):
     """Схема для создания пользователя"""
     username: str
     password: str
-    email: EmailStr
+    email: str
 
     @validator("email")
-    def validate_email(cls, email):
-        validate_email(email)
-        return email.lower()
+    def validate_email(cls, value):
+        """Валидация поля email"""
+        try:
+            validate_email(value)
+        except errors.EmailError:
+            return {"message": "Email is not valid"}
+        return value.lower()
+
+    @validator("username")
+    def validate_username(cls, value):
+        """Валидация поля username"""
+        if len(value) < 3:
+            return {"message": f"The {value} is incorrect, the name must be at least 3 characters long"}
+        elif len(value) > 35:
+            return {"message": f"The {value} is incorrect, the length of the name must exceed 35 characters"}
+        return value
+
+    @validator("password")
+    def validate_password(cls, value):
+        """Валидация поля password"""
+        if len(value) < 7:
+            return {"message": "Password must be at least 7 characters long"}
+        elif not value.isalnum():
+            return {"message": "Password must contain only letters and numbers"}
+        return value
+
+    @classmethod
+    def as_form(cls, username: str = Form(...), password: str = Form(...), email: str = Form(...)):
+        return cls(username=username, password=password, email=email)
 
     class Config:
         orm_mode = True
