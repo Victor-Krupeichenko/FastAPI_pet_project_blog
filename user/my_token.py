622d2a0399da2cfaf0d142f62cf58b62834831df
Victor-Krupeichenko <71899727+Victor-Krupeichenko@users.noreply.github.com>
2023-06-28 21:46:56 +0300

    Исправлено получение текущего пользователя

--- user/my_token.py
+++ user/my_token.py
@@ -5,14 +5,14 @@ from src.settings_env import SECRET_KEY_TOKEN, ALGORITHM_TOKEN
 from datetime import datetime, timedelta
 from sqlalchemy import select
 from src.api_models import User
-from user.token_from_cookies import OAuth2PasswordBearerWithCookie
 from api_databases.connect_db import get_async_session
 from sqlalchemy.ext.asyncio import AsyncSession
 
 ACCESS_TOKEN_EXPIRE_DAYS = 1
 NAME_COOKIES = "my_app_cookies"
 
-oauth2_scheme = OAuth2PasswordBearerWithCookie(token_url="/user/login", cookies=NAME_COOKIES)
+
+# oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/user/login")
 
 
 def create_access_token(data: dict):
@@ -22,28 +22,23 @@ def create_access_token(data: dict):
     return encoded_jwt  # Возвращаем закодированный токен
 
 
-async def get_current_user(request: Request,
-                           session: AsyncSession = Depends(get_async_session),
-                           protect: str = Depends(oauth2_scheme)
-                           ):
+async def get_current_user(request: Request, session: AsyncSession = Depends(get_async_session)):
     """Получение текущего пользователя"""
-    credentials_exception = HTTPException(
-        status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="Could not validate credentials",
-    )
+    credentials_exception = None
     try:
         token = request.cookies.get(NAME_COOKIES)
-        scheme = token.split(" ")[1]
-        payload = jwt.decode(scheme, key=SECRET_KEY_TOKEN, algorithms=ALGORITHM_TOKEN)
-        username = payload.get("sub")
-        if username is None:
-            raise credentials_exception
+        if token is not None:
+            scheme = token.split(" ")[1]
+            payload = jwt.decode(scheme, key=SECRET_KEY_TOKEN, algorithms=ALGORITHM_TOKEN)
+            username = payload.get("sub")
+        else:
+            return credentials_exception
     except JWTError:
-        raise credentials_exception
+        return credentials_exception
     user = select(User.username, User.group, User.is_active, User.id, User.email).select_from(User).filter(
         User.username == username)
     if user is None:
-        raise credentials_exception
+        return credentials_exception
     temporary = await session.execute(user)
     try:
         temporary_user = temporary.all()[0]  # Попадаем внутрь списка
