5b41f9c5154aef709cf763013a5a2c55f83d0c66
Victor-Krupeichenko <71899727+Victor-Krupeichenko@users.noreply.github.com>
2023-06-28 21:49:48 +0300

    Схема для создания пользователя

--- user/schemas.py
+++ user/schemas.py
@@ -4,28 +4,18 @@ from pydantic import BaseModel, validator, errors
 from pydantic.networks import validate_email
 
 
-class UserSchema(BaseModel):
-    """Схема для создания пользователя"""
+class AuthUserScheme(BaseModel):
+    """Схема для аутентификации пользователя"""
     username: str
     password: str
-    email: str
-
-    @validator("email")
-    def validate_email(cls, value):
-        """Валидация поля email"""
-        try:
-            validate_email(value)
-        except errors.EmailError:
-            return {"message": "Email is not valid"}
-        return value.lower()
 
     @validator("username")
     def validate_username(cls, value):
         """Валидация поля username"""
         if len(value) < 3:
-            return {"message": f"The {value} is incorrect, the name must be at least 3 characters long"}
+            return {"message": f"The name: {value} is incorrect, the name must be at least 3 characters long"}
         elif len(value) > 35:
-            return {"message": f"The {value} is incorrect, the length of the name must exceed 35 characters"}
+            return {"message": f"The name: {value} is incorrect, the length of the name must exceed 35 characters"}
         return value
 
     @validator("password")
@@ -36,6 +26,24 @@ class UserSchema(BaseModel):
         elif not value.isalnum():
             return {"message": "Password must contain only letters and numbers"}
         return value
+
+    @classmethod
+    def as_form(cls, username: str = Form(...), password: str = Form(...)):
+        return cls(username=username, password=password)
+
+
+class UserSchema(AuthUserScheme):
+    """Схема для создания пользователя"""
+    email: str
+
+    @validator("email")
+    def validate_email(cls, value):
+        """Валидация поля email"""
+        try:
+            validate_email(value)
+        except errors.EmailError:
+            return {"message": "Email is not valid"}
+        return value.lower()
 
     @classmethod
     def as_form(cls, username: str = Form(...), password: str = Form(...), email: str = Form(...)):
