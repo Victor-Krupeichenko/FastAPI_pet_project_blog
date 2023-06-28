0b46c98d90400d303214b17629fd6f5409feeb18
Victor-Krupeichenko <71899727+Victor-Krupeichenko@users.noreply.github.com>
2023-06-28 21:51:03 +0300

    Авторизация пользователя, выход пользователя, удаление пользователя

--- webapp/routers/routers.py
+++ webapp/routers/routers.py
@@ -1,6 +1,7 @@
-from fastapi import APIRouter, Request, Depends, responses, status
+from fastapi import APIRouter, Request, Depends, status, responses
 from fastapi.templating import Jinja2Templates
 from api_databases.connect_db import PAGE
+from user.my_token import get_current_user
 from post.routers import (
     get_all_posts,
     get_one_post,
@@ -11,8 +12,13 @@ from category.routers import (
     get_category
 )
 from user.routers import (
-    user_create
+    user_create,
+    login,
+    user_logout,
+    delete_user,
+    update_user
 )
+from user.my_token import NAME_COOKIES
 
 router = APIRouter(include_in_schema=False)
 templates = Jinja2Templates(directory="webapp/templates")
@@ -37,30 +43,31 @@ env.filters["word_count"] = word_count
 
 @router.get("/")
 async def post_all(request: Request, posts=Depends(get_all_posts), categories=Depends(get_all_categories),
-                   page: int = PAGE, messages: str = None):
+                   page: int = PAGE, messages: str = None, current_user=Depends(get_current_user)):
     """Главная страница (вывод всех постов) + пагинация """
-    return templates.TemplateResponse("index.html",
-                                      {"request": request, "posts": posts["data"], "total_pages": posts["total_pages"],
-                                       "categories": categories, "page": page,
-                                       "show_pagination": posts["show_pagination"], "messages": messages})
+    return templates.TemplateResponse("index.html", {
+        "request": request, "posts": posts, "categories": categories, "page": page, "messages": messages,
+        "current_user": current_user
+    })
 
 
 @router.get("/one_post/{post_id}")
-def one_post(request: Request, post=Depends(get_one_post), categories=Depends(get_all_categories)):
+def one_post(request: Request, post=Depends(get_one_post), categories=Depends(get_all_categories),
+             current_user=Depends(get_current_user)):
     """Страница детальной информации о записи"""
-    return templates.TemplateResponse("detail_post.html",
-                                      {"request": request, "post": post, "categories": categories})
+    return templates.TemplateResponse("detail_post.html", {
+        "request": request, "post": post, "categories": categories, "current_user": current_user
+    })
 
 
 @router.get('/category_post_all/{category_id}')
 def category_post_all(request: Request, posts=Depends(category_post_all), categories=Depends(get_all_categories),
-                      category_title=Depends(get_category), page: int = PAGE):
+                      category_title=Depends(get_category), page: int = PAGE, current_user=Depends(get_current_user)):
     """Вывод всех записей у конкретной категории + пагинация"""
-    return templates.TemplateResponse("index.html",
-                                      {"request": request, "posts": posts["data"], "total_pages": posts["total_pages"],
-                                       "categories": categories, "page": page,
-                                       "show_pagination": posts["show_pagination"],
-                                       "category_title": category_title})
+    return templates.TemplateResponse("index.html", {
+        "request": request, "posts": posts, "categories": categories, "page": page, "category_title": category_title,
+        "current_user": current_user
+    })
 
 
 message = "User Register"
@@ -68,13 +75,68 @@ message = "User Register"
 
 @router.get("/registration")
 def registration(request: Request):
-    return templates.TemplateResponse("user_create.html", {"request": request, "msg": message})
+    """Переход на страницу регистрации"""
+    return templates.TemplateResponse("user_auth.html", {"request": request, "msg": message, "logup": True})
 
 
 @router.post('/registration')
 def registration(request: Request, user=Depends(user_create)):
     """Регистрация пользователя"""
     if "errors" in user:
-        return templates.TemplateResponse("user_create.html",
-                                          {"request": request, "msg": message, "errors": user["errors"]})
+        return templates.TemplateResponse("user_auth.html", {
+            "request": request, "msg": message, "errors": user["errors"], "logup": True
+        })
+    return responses.RedirectResponse('/?messages=Successfully Registration', status_code=status.HTTP_302_FOUND)
+
+
+msg = "User Login"
+
+
+@router.get("/login")
+def log_in(request: Request):
+    """Переход на страницу авторизации"""
+    return templates.TemplateResponse("user_auth.html", {"request": request, "msg": msg, "log_in": True})
+
+
+@router.post("/login")
+def log_in(request: Request, user=Depends(login)):
+    """Авторизация пользователя"""
+    if "errors" in user:
+        return templates.TemplateResponse("user_auth.html", {
+            "request": request, "errors": user["errors"], "log_in": True, "msg": msg
+        })
+    response = responses.RedirectResponse(
+        f'/?messages=User: {user["username"]} is authorized', status_code=status.HTTP_302_FOUND
+    )
+    response.set_cookie(key=NAME_COOKIES, value=f'Bearer {user["token"]}', httponly=True)
+    return response
+
+
+@router.get("/logout")
+def log_out(user=Depends(user_logout)):
+    """Выход пользователя"""
+    response = responses.RedirectResponse(f'/?messages={user["message"]}', status_code=status.HTTP_302_FOUND)
+    response.delete_cookie(NAME_COOKIES)
+    return response
+
+
+@router.get("/delete/{user_id}", dependencies=[Depends(delete_user)])
+def user_delete():
+    """Удаление пользователя"""
+    response = responses.RedirectResponse(f'/', status_code=status.HTTP_302_FOUND)
+    response.delete_cookie(NAME_COOKIES)
+    return response
+
+
+@router.get("/update_user")
+def user_update(request: Request, current_user=Depends(get_current_user)):
+    up_user = "Update User"
+    return templates.TemplateResponse("user_auth.html", {
+        "request": request, "msg": up_user, "update_user": True, "current_user": current_user
+    })
+
+
+# Обновление пользователя (пока в разработке)
+@router.post('/update_user/{user_id}')
+def user_update(request: Request, user=Depends(update_user)):
     return responses.RedirectResponse('/?messages=Successfully Registration', status_code=status.HTTP_302_FOUND)
