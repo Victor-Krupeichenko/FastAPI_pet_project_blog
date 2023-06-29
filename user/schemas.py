ddc5975d1cdcaa2a302250ca4d4749773aa17dc7
Victor-Krupeichenko <71899727+Victor-Krupeichenko@users.noreply.github.com>
2023-06-29 20:55:23 +0300

    Добавлен еще один ключ для возврата значения

--- user/schemas.py
+++ user/schemas.py
@@ -13,18 +13,26 @@ class AuthUserScheme(BaseModel):
     def validate_username(cls, value):
         """Валидация поля username"""
         if len(value) < 3:
-            return {"message": f"The name: {value} is incorrect, the name must be at least 3 characters long"}
+            return {"message": f"The name: {value} is incorrect, the name must be at least 3 characters long",
+                    "value": value
+                    }
         elif len(value) > 35:
-            return {"message": f"The name: {value} is incorrect, the length of the name must exceed 35 characters"}
+            return {"message": f"The name: {value} is incorrect, the length of the name must exceed 35 characters",
+                    "value": value
+                    }
         return value
 
     @validator("password")
     def validate_password(cls, value):
         """Валидация поля password"""
         if len(value) < 7:
-            return {"message": "Password must be at least 7 characters long"}
+            return {"message": "Password must be at least 7 characters long",
+                    "value": value
+                    }
         elif not value.isalnum():
-            return {"message": "Password must contain only letters and numbers"}
+            return {"message": "Password must contain only letters and numbers",
+                    "value": value
+                    }
         return value
 
     @classmethod
@@ -42,7 +50,9 @@ class UserSchema(AuthUserScheme):
         try:
             validate_email(value)
         except errors.EmailError:
-            return {"message": "Email is not valid"}
+            return {"message": "Email is not valid",
+                    "value": value
+                    }
         return value.lower()
 
     @classmethod
