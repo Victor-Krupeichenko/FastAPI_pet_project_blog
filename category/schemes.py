6630479c736b3b42b41dd331f34551725ab27059
Victor-Krupeichenko <71899727+Victor-Krupeichenko@users.noreply.github.com>
2023-06-30 16:38:47 +0300

    Схема валидации поля у категории

--- category/schemes.py
+++ category/schemes.py
@@ -1,8 +1,22 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, validator
+from fastapi import Form
 
 
 class CategoryScheme(BaseModel):
+    """Схема валидации поля у категории"""
     title: str
+
+    @validator("title")
+    def validate_title(cls, value):
+        if len(value) < 3:
+            return {
+                "message": f"category: {value} is incorrect, the title category must be at least 3 characters long",
+                "value": value}
+        return value
+
+    @classmethod
+    def as_form(cls, title: str = Form(...)):
+        return cls(title=title)
 
     class Config:
         orm_mode = True
