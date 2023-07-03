f7170d9979657f39dcbfeb85ca8039ce61199319
Victor-Krupeichenko <71899727+Victor-Krupeichenko@users.noreply.github.com>
2023-07-03 16:35:13 +0300

    Кэширование

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from fastapi_cache import FastAPICache
+from fastapi_cache.backends.redis import RedisBackend
 from category.routers import router as router_category
 from user.routers import router as router_user
 from post.routers import router as post_router
@@ -7,10 +9,23 @@ from fastapi.staticfiles import StaticFiles
 # pip install itsdangerous
 from starlette.middleware.sessions import SessionMiddleware
 from src.settings_env import SECRET_KEY_SESSION
+from src.settings_env import REDIS_HOST
+from redis import asyncio as aioredis
 
 app = FastAPI(
     title="My_app_project"
 )
+
+
+# Кеширование
+# pip install "fastapi-cache2[redis]"
+@app.on_event("startup")
+async def startup():
+    """При старте проекта подключается к redis для кэширования"""
+    redis = aioredis.from_url(f"redis://{REDIS_HOST}", encoding="utf8", decode_responses=True)
+    FastAPICache.init(RedisBackend(redis), prefix="fastapi-cache")
+
+
 # В сессии будем хранить сообщение для вывода при перенаправлении
 app.add_middleware(SessionMiddleware, secret_key=SECRET_KEY_SESSION)
 
