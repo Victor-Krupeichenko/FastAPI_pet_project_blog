39469e747d8f45bc82daeebd654cddbb5e9edc22
Victor-Krupeichenko <71899727+Victor-Krupeichenko@users.noreply.github.com>
2023-07-03 13:08:21 +0300

    Добавлены настройки для SMTP-сервера, Redis

--- src/settings_env.py
+++ src/settings_env.py
@@ -13,3 +13,14 @@ SECRET_KEY_TOKEN = os.getenv("SECRET_KEY_TOKEN")
 ALGORITHM_TOKEN = os.getenv("ALGORITHM_TOKEN")
 
 SECRET_KEY_SESSION = os.getenv("SECRET_KEY_SESSION")
+
+REDIS_HOST = os.getenv("REDIS_HOST")
+REDIS_PORT = os.getenv("REDIS_PORT")
+
+SMTP_USER = os.getenv("SMTP_USER")
+SMTP_PASSWORD = os.getenv("SMTP_PASSWORD")
+SMTP_HOST = os.getenv("SMTP_HOST")
+SMTP_PORT = os.getenv("SMTP_PORT")
+
+URL_HOST = os.getenv("URL_HOST")  # Хост приложения
+URL_PORT = os.getenv("URL_PORT")  # Порт приложения
